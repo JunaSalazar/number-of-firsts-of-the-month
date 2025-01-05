7858127eb9d9636fc732df4112ccc1484de5b36b
Juna Isaac Salazar Meza <juanisaacsm@gmail.com>
2025-01-05 15:32:37 -0600

    Se ordeno el codigo de leer archivo, conteo y sort, en una funcion independiente llamado read_file(), se creo funcion get_month_and_year() para obtener el mes y año local para definir el nombre del archivo, se declaro el metodo write_file(), pero este ultimo sigue vacio

--- main.py
+++ main.py
@@ -1,62 +1,72 @@
-#! -*- coding: utf-8 -*-
-
 import io
 import time
 import re
 import os, fnmatch
 import sys
+import locale
+from datetime import datetime
 
-# This is a sample Python script.
+list = []
+nameCount = []
+indiviualName = []
+data = []
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Metodo donde se abre el archivo INI, se obtiene la informacion
+# y se realiza el conteo para obtener los nombres y la cantidad de veces que estos se repiten
+# almacenandolos en una lista, y ordenandolos de manera decendnente por el numero en la segunda columna utilizando
+# la funcion LAMBDA.
+def read_file():
+    file1 = open("file.ini", 'r')
+    lines = file1.readlines()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-
-file1 = open("file.ini", 'r')
-lines = file1.readlines()
-try:
     list = []
     nameCount = []
     indiviualName = []
     data = []
-    print("Enter Try")
-    for line in reversed(lines):
-        list = re.split(r"\t+", line)
 
-    linea = list[0]
-    linea = linea.replace("[","")
-    linea = linea.replace("]","")
-    names = linea.split(",")
+    try:
+        for line in reversed(lines):
+            list = re.split(r"\t+", line)
 
-    for name in names:
-        if (not indiviualName):
-            indiviualName.append(name)
-            nameCount.append(1)
-        elif (name in indiviualName):
-            nameCount[indiviualName.index(name)] += 1
-        else:
-            indiviualName.append(name)
-            nameCount.append(1)
+        linea = list[0]
+        linea = linea.replace("[", "")
+        linea = linea.replace("]", "")
+        names = linea.split(",")
 
-    for name in indiviualName:
-        data.append([name, nameCount[indiviualName.index(name)]])
+        for name in names:
+            if (not indiviualName):
+                indiviualName.append(name)
+                nameCount.append(1)
+            elif (name in indiviualName):
+                nameCount[indiviualName.index(name)] += 1
+            else:
+                indiviualName.append(name)
+                nameCount.append(1)
 
-    data = sorted(data, key=lambda data: data[1], reverse = True)
-    print(data)
-except Exception as e:
-    # Closing file
-    print("Fallo: ",e)
-    file1.close()
+        for name in indiviualName:
+            data.append([name, nameCount[indiviualName.index(name)]])
+
+        data = sorted(data, key=lambda data: data[1], reverse=True)
+        return data
+    except Exception as e:
+        # Closing file
+        print("Fallo: ", e)
+        file1.close()
+
+# Metodo para obtener el mes y el año actual, se escribe de la siguiente manera MESAÑO
+def get_month_and_year():
+    date_month = datetime.today().strftime('%b').upper()
+    date_month = date_month.replace(".","")
+    date_year = datetime.today().strftime('%Y')
+    date = date_month+date_year
+    return date
+
+# Metodo para escribir archivo, si existe el archivo se re-escribe, si no existe se crea uno nuevo
+def write_file(data):
+    print("")
+
+locale.setlocale(locale.LC_ALL, ("es_ES", "UTF-8"))
+data = read_file()
+write_file(data)
+print(data)
+
